991f38c4b7acfdb1b0c53c51f04ea4912e1e8395
sumitjat <sumitjat56gmail.com>
2019-03-12 18:18:19 +0530

    Marker working fine in customer map though added new feature shows distance between user and driver

--- app/src/main/java/temperatureconverter/android/vogella/com/olauber/CustomerSettingActivity.java
+++ app/src/main/java/temperatureconverter/android/vogella/com/olauber/CustomerSettingActivity.java
@@ -1,19 +1,36 @@
 package temperatureconverter.android.vogella.com.olauber;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.storage.StorageManager;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 
+import com.bumptech.glide.Glide;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.sql.Struct;
 import java.util.HashMap;
 import java.util.Map;
@@ -23,6 +40,9 @@ public class CustomerSettingActivity extends AppCompatActivity {
 
     private Button mconfirm,mback;
     private EditText mname,mphone;
+    private ImageView imageView;
+    private Uri resulturi;
+    private String profileimageurl;
 
     private FirebaseAuth mAuth;
     private DatabaseReference mcustomerdatabaseref;
@@ -36,6 +56,7 @@ public class CustomerSettingActivity extends AppCompatActivity {
 
         mconfirm=findViewById(R.id.confirmbttn);
         mback=findViewById(R.id.backbttn);
+        imageView=findViewById(R.id.profileImage);
 
         mname=findViewById(R.id.Name);
         mphone=findViewById(R.id.phone);
@@ -46,7 +67,15 @@ public class CustomerSettingActivity extends AppCompatActivity {
         mcustomerdatabaseref= FirebaseDatabase.getInstance().getReference().child("Users").child("Customers").child(userID);
         getUser();
 
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
+                Intent intent=new Intent(Intent.ACTION_PICK);
+                intent.setType("image/*");
+                startActivityForResult(intent,1);
+            }
+        });
         mconfirm.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -73,6 +102,48 @@ public class CustomerSettingActivity extends AppCompatActivity {
         userinfo.put("name",Name);
         userinfo.put("phone",Phone);
         mcustomerdatabaseref.updateChildren(userinfo);
+
+        if(resulturi!=null){
+
+            StorageReference storageReference= FirebaseStorage.getInstance().getReference().child("profile_images").child(userID);
+            Bitmap bitmap=null;
+
+            try {
+                bitmap= MediaStore.Images.Media.getBitmap(getContentResolver(),resulturi);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            ByteArrayOutputStream boas=new ByteArrayOutputStream();
+            bitmap.compress(Bitmap.CompressFormat.JPEG,20,boas);
+            byte[] data=boas.toByteArray();
+            UploadTask uploadTask=storageReference.putBytes(data);
+
+            uploadTask.addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    finish();
+                    return;
+                }
+            });
+
+            uploadTask.addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    Uri downlaoduri=taskSnapshot.getUploadSessionUri(); //getdownload uri is not present in this
+                    Map newimage=new HashMap();
+                    newimage.put("profileimageurl",downlaoduri.toString());
+                    mcustomerdatabaseref.updateChildren(newimage);
+
+                    finish();
+                    return;
+
+                }
+            });
+        }
+        else {
+            finish();
+        }
 
         finish();
     }
@@ -101,7 +172,14 @@ public class CustomerSettingActivity extends AppCompatActivity {
                         mphone.setText(Phone);
 
                     }
-                }
+
+                    if (map.get("profileimageurl")!=null)
+                    {
+                        profileimageurl=map.get("profileimageurl").toString();
+                        Glide.with(getApplication()).load(profileimageurl).into(imageView);
+
+
+                    }                }
             }
 
             @Override
@@ -109,5 +187,16 @@ public class CustomerSettingActivity extends AppCompatActivity {
 
             }
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode==1 && resultCode== Activity.RESULT_OK)
+        {
+            final Uri imageuri=data.getData();
+            resulturi=imageuri;
+            imageView.setImageURI(resulturi);
+        }
     }
 }
